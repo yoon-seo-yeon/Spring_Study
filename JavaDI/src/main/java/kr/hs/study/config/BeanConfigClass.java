bdcf540a27a1cb4faeba13f72ecc8e2d93b46178
yoon-seo-yeon <ysy02091234@gmail.com>
2022-04-13 09:32:31 +0900

    JavaDI

---
+++ JavaDI/src/main/java/kr/hs/study/config/BeanConfigClass.java
@@ -0,0 +1,24 @@
+package kr.hs.study.config;
+
+import org.springframework.context.annotation.Bean;
+
+import kr.hs.study.beans.DataBean1;
+import kr.hs.study.beans.TestBean1;
+
+public class BeanConfigClass {
+	@Bean
+	public TestBean1 java1() {
+		TestBean1 t1 = new TestBean1(200, "spring2", new DataBean1());
+		return t1;
+	}
+
+	@Bean
+	public TestBean1 java2() {
+		TestBean1 t2 = new TestBean1();
+		t2.setData1(400);
+		t2.setData2("spring4");
+		t2.setData3(new DataBean1());
+		
+		return t2;
+	}
+}
